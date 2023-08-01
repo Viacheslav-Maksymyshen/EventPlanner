85dc19c7352f8b620d66b453b37c1b2c781267b9
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2023-08-01 21:59:33 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreateEventPage from "./pages/CreateEventPage";
+import EventPage from "./pages/EventPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Головна</Link>
+          </li>
+          <li>
+            <Link to="/create">Створити подію</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateEventPage />} />
+        <Route path="/event/:eventId" element={<EventPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Сторінку не знайдено</h1>
+      {/* Відображення повідомлення про те, що сторінку не знайдено */}
+    </div>
+  );
+};
+
+export default NotFoundPage;
