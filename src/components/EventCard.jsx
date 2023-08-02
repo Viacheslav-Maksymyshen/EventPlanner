112685f7979d892ab6a792bc18c6f5bacc3257ab
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2023-08-02 16:42:49 +0300

    added event

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Box, Text, Button } from "@chakra-ui/react";
 
-const EventCard = () => {
-  // Відображення інформації про подію (наприклад, відображення одного елемента списку)
-  return <div>Event Card</div>;
+const EventCard = ({ event }) => {
+  return (
+    <Box
+      p="4"
+      borderWidth="1px"
+      borderRadius="md"
+      boxShadow="md"
+      position="relative"
+    >
+      <Text fontSize="xl" fontWeight="bold" mb="2">
+        {event.title}
+      </Text>
+      <Text>Date: {event.date}</Text>
+      <Text>Time: {event.time}</Text>
+      {/* Button займає всю площу батьківського Box */}
+      <Button
+        position="absolute"
+        top="0"
+        left="0"
+        width="100%"
+        height="100%"
+        zIndex="1" // Встановлюємо zIndex, щоб кнопка була доступна для кліку
+        opacity="0" // Прозорість для того, щоб кнопка була невидимою, але доступною для кліку
+      >
+        {/* Тут додати Link компонент або виклик функції для переходу на сторінку */}
+      </Button>
+    </Box>
+  );
 };
 
 export default EventCard;
--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import eventsData from "../data/eventsData";
 
 const EventPage = () => {
+  const { eventId } = useParams();
+  const event = eventsData.find((event) => event.id === parseInt(eventId));
+
+  if (!event) {
+    return <Box p="6">Подію не знайдено</Box>;
+  }
+
   return (
-    <div>
-      <h1>Назва події</h1>
-      {/* Детальна інформація про подію */}
-    </div>
+    <Box p="6">
+      <Heading as="h1" size="xl" mb="4">
+        {event.title}
+      </Heading>
+      <Text>Дата: {event.date}</Text>
+      <Text>Час: {event.time}</Text>
+      <Text>Місце: {event.location}</Text>
+      <Text>Опис: {event.description}</Text>
+    </Box>
   );
 };
 
