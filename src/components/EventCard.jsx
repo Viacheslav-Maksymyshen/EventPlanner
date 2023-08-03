e59c780093ace166a3bf5a72944dc7396c385cd1
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2023-08-03 17:02:01 +0300

    added button more info

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,32 +1,25 @@
+// src/components/EventCard.jsx
 import React from "react";
-import { Box, Text, Button } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const EventCard = ({ event }) => {
   return (
     <Box
       p="4"
-      borderWidth="1px"
+      border="1px solid #ddd"
       borderRadius="md"
       boxShadow="md"
+      transition="box-shadow 0.3s"
+      _hover={{ boxShadow: "lg" }}
       position="relative"
     >
-      <Text fontSize="xl" fontWeight="bold" mb="2">
+      <Heading as="h2" size="md" mb="2">
         {event.title}
-      </Text>
+      </Heading>
       <Text>Date: {event.date}</Text>
       <Text>Time: {event.time}</Text>
-      {/* Button займає всю площу батьківського Box */}
-      <Button
-        position="absolute"
-        top="0"
-        left="0"
-        width="100%"
-        height="100%"
-        zIndex="1" // Встановлюємо zIndex, щоб кнопка була доступна для кліку
-        opacity="0" // Прозорість для того, щоб кнопка була невидимою, але доступною для кліку
-      >
-        {/* Тут додати Link компонент або виклик функції для переходу на сторінку */}
-      </Button>
+      <Text>Location: {event.location}</Text>
+      <Text>Description: {event.description}</Text>
     </Box>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,54 @@
+// src/pages/HomePage.jsx
 import React from "react";
-import { Box, Heading, Grid } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Grid, Button } from "@chakra-ui/react";
 import EventCard from "../components/EventCard";
+
+import { Link } from "react-router-dom";
 
 const HomePage = ({ events }) => {
   return (
-    <Box p="6">
-      <Heading as="h1" size="xl" mb="6">
-        Список найближчих подій
-      </Heading>
-      <Grid templateColumns="repeat(auto-fill, minmax(300px, 1fr))" gap="6">
-        {events.map((event) => (
-          <Link key={event.id} to={`/event/${event.id}`}>
-            <EventCard event={event} />
-          </Link>
-        ))}
-      </Grid>
-    </Box>
+    <Grid templateColumns="repeat(auto-fit, minmax(250px, 1fr))" gap="4">
+      {events.map((event) => (
+        <EventCardWrapper key={event.id} event={event} />
+      ))}
+    </Grid>
+  );
+};
+
+const EventCardWrapper = ({ event }) => {
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{ position: "relative" }} // Додаємо властивість для розміщення абсолютно позиціонованої кнопки
+    >
+      <EventCard event={event} />
+      {isHovered && (
+        <Link to={`/event/${event.id}`}>
+          <Button
+            position="absolute"
+            bottom="4"
+            left="50%"
+            transform="translateX(-50%)"
+            opacity="0"
+            transition="opacity 0.3s"
+            _hover={{ opacity: "1" }}
+          >
+            More info
+          </Button>
+        </Link>
+      )}
+    </div>
   );
 };
 
